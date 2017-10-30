1bfe0803f82409170e7bbc59269a447ff2292d83
SOM ON Soum <somonmel07@gmail.com>
2017-10-31 00:17:49 +0700

    Fourth commit

--- src/components/component/component.ts
+++ src/components/component/component.ts
@@ -1,11 +1,6 @@
 import { Component } from '@angular/core';
 
-/**
- * Generated class for the Component component.
- *
- * See https://angular.io/api/core/Component for more info on Angular
- * Components.
- */
+
 @Component({
   selector: 'component',
   templateUrl: 'component.html'
